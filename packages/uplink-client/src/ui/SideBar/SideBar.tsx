9e68bf35fbfdb001dbf913c2ae399c7bb4be1558
jooshwelsh <josh_welsh10@yahoo.com>
2023-02-21 18:39:34 -0500

    resize logo, temp styling

--- packages/uplink-client/src/ui/SideBar/SideBar.tsx
+++ packages/uplink-client/src/ui/SideBar/SideBar.tsx
@@ -3,32 +3,30 @@ import uplinkLogo from "../../../public/uplink-logo.svg";
 
 export default function Sidebar() {
   return (
-    <div className="text-3xl text-green-600 p-2 place-self bg-purple-500">
-      <div className="">
-        <Image src={uplinkLogo} alt="uplink logo" width={50} height={50} />
+    <div className="flex flex-col bg-neutral x-0 y-0">
+      <div className="py-6 pl-4">
+        <Image src={uplinkLogo} alt="uplink logo" width={150}/>
       </div>
-      <hr className="bg-gray-200 border border-gray-200 rounded-full mx-2" />
-
-      <div
-        className="flex items-center justify-center 
-                        h-12 w-12 mt-2 mb-2 mx-auto  
-                        bg-gray-400 hover:bg-green-600 dark:bg-gray-800 
-                        text-green-500 hover:text-white
+      <div className="flex flex-col h-full w-fit m-auto text-3xl">
+        <div
+          className="flex items-center justify-center 
+                        h-16 w-16 mt-2 mb-2 
+                        bg-gray-600 hover:bg-primary 
+                        text-white
                         hover:rounded-xl rounded-3xl
                         transition-all duration-300 ease-linear
                         cursor-pointer shadow-lg
                         tooltip"
-        data-tip="New Org"
-      >
-        <p>+</p>
-      </div>
-      <hr className="bg-gray-200 border border-gray-200 rounded-full mx-2" />
-      <div className="avatar">
+          data-tip="New Org"
+        >
+          <p>+</p>
+        </div>
+        <hr className="bg-gray-200 border border-gray-200 rounded-full mx-2" />
         <div
-          className="relative flex items-center justify-center 
-                        h-12 w-12 mt-2 mb-2 mx-auto  
-                    bg-gray-400 hover:bg-green-600 dark:bg-gray-800 
-                    text-green-500 hover:text-white
+          className="avatar flex items-center justify-center 
+                        h-16 w-16 mt-2 mb-2  
+                    bg-gray-400 
+                    text-green-500
                         hover:rounded-xl rounded-3xl
                         transition-all duration-300 ease-linear
                         cursor-pointer shadow-lg"
