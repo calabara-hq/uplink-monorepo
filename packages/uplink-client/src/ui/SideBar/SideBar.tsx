64c1ca9402314e7c0d0dcae93c50a6391c9455af
jooshwelsh <josh_welsh10@yahoo.com>
2023-02-21 16:35:11 -0500

    add temp org avatar

--- packages/uplink-client/src/ui/SideBar/SideBar.tsx
+++ packages/uplink-client/src/ui/SideBar/SideBar.tsx
@@ -1,12 +1,8 @@
-
-
-
-
-
+import Image from "next/image";
 
 export default function Sidebar() {
   return (
-    <div className="text-3xl text-green-600 p-2 absolute inset-y-0 left-0">
+    <div className="text-3xl text-green-600 p-2 absolute inset-y-8 left-0 bg-purple-500">
       <div
         className="relative flex items-center justify-center 
                         h-12 w-12 mt-2 mb-2 mx-auto  
@@ -16,18 +12,26 @@ export default function Sidebar() {
                         transition-all duration-300 ease-linear
                         cursor-pointer shadow-lg"
       >
-        <p>-</p>
+        <p>+</p>
       </div>
-      <div
-        className="relative flex items-center justify-center 
+      <hr className="bg-gray-200 border border-gray-200 rounded-full mx-2" />
+      <div className="avatar">
+        <div
+          className="relative flex items-center justify-center 
                         h-12 w-12 mt-2 mb-2 mx-auto  
                     bg-gray-400 hover:bg-green-600 dark:bg-gray-800 
                     text-green-500 hover:text-white
                         hover:rounded-xl rounded-3xl
                         transition-all duration-300 ease-linear
                         cursor-pointer shadow-lg"
-      >
-        <p>+</p>
+        >
+          <Image
+            src={"/noun-47.png"}
+            alt={"org avatar"}
+            height={50}
+            width={50}
+          />
+        </div>
       </div>
     </div>
   );
