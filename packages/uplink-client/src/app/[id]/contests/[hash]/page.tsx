210840e217689a0135f4554222486da07bb7a6dd
jooshwelsh <josh_welsh10@yahoo.com>
2023-03-22 15:16:46 -0400

    fix detail/requirement layout

--- packages/uplink-client/src/app/[id]/contests/[hash]/page.tsx
+++ packages/uplink-client/src/app/[id]/contests/[hash]/page.tsx
@@ -135,9 +135,9 @@ export default async function Page({ params }: { params: { id: string } }) {
         </div>
       </div>
 
-      <div className="lg:flex lg:flex-row lg:items-start lg:justify-evenly w-full items-center gap-4 bg-neutral">
-        <div className="lg:flex-row lg:flex-1 flex flex-col-reverse items-center w-full bg-base-100 shadow-xl rounded-xl">
-          <div className="flex flex-col justify-evenly lg:w-2/3 h-96 p-4">
+      <div className="lg:flex lg:flex-row w-full items-center gap-4 bg-neutral">
+        <div className="lg:flex-row flex flex-col-reverse items-center lg:w-3/4 bg-base-100 shadow-xl rounded-xl">
+          <div className="flex flex-col lg:flex-auto lg:w-1/2 gap-4 p-4">
             <div className="avatar">
               <div className="w-20 lg:w-24 rounded-full bg-white mr-6">
                 <Image
@@ -161,21 +161,19 @@ export default async function Page({ params }: { params: { id: string } }) {
               </label>
             </div>
           </div>
-          <div className="max-w-screen-sm lg:w-[] lg:h-96 ml-auto">
+          <div className="max-w-screen-sm lg:h-96">
             <Image
               src={contestImage}
               alt="contest image"
-              height={400}
-              width={400}
               className="h-full w-full object-cover rounded-xl"
             />
           </div>
         </div>
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col items-center gap-4">
-            <div className="hidden lg:grid lg:grid-cols-2 w-full gap-6 p-4 bg-base-100 rounded-xl ">
+        <div className="flex flex-col justify-start lg:h-96 w-1/4 gap-4">
+          <div className="hidden lg:flex lg:flex-col gap-4 p-4 bg-base-100 rounded-xl">
+            <div className="flex flex-row justify-between items-center">
               <p className="text-xl">Details</p>
-              <div className="btn btn-sm btn-circle ml-auto">
+              <div className="btn btn-sm btn-circle">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -191,102 +189,116 @@ export default async function Page({ params }: { params: { id: string } }) {
                   />
                 </svg>
               </div>
+            </div>
+            <div className="flex flex-row justify-between items-center">
               <p>Voting Begins</p>
-              <p className="ml-auto">3/11/23</p>
+              <p>3/11/23</p>
+            </div>
+            <div className="flex flex-row justify-between items-center">
               <p>Contest End</p>
-              <p className="ml-auto">3/13/23</p>
+              <p>3/13/23</p>
             </div>
           </div>
-          <div className="hidden lg:grid lg:grid-cols-2 items-center gap-6 p-4 bg-base-100 rounded-xl">
-            <p className="text-xl">Requirements</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6 ml-auto"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-              />
-            </svg>
+          <div className="hidden lg:flex lg:flex-col gap-4 p-4 bg-base-100 rounded-xl">
+            <div className="flex flex-row justify-between items-center">
+              <p className="text-xl">Requirements</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                />
+              </svg>
+            </div>
 
-            <p>Wallet</p>
-            <div className="flex flex-row items-center gap-2 ml-auto">
-              <p>0x44..BBC0</p>
-              <div className="btn btn-sm btn-success btn-circle ml-auto">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
-                  />
-                </svg>
+            <div className="flex flex-row justify-between items-center">
+              <p>Wallet</p>
+              <div className="flex flex-row items-center gap-2">
+                <p>0x44..BBC0</p>
+                <div className="btn btn-sm btn-success btn-circle ml-auto">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
-            <p>Token Check</p>
-            <div className="flex flex-row items-center gap-2 ml-auto">
-              <div className="dropdown">
-                <label tabIndex={0} className="hover:cursor-pointer">
-                  Eligible
-                </label>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content menu p-2 shadow bg-neutral rounded-box w-48"
-                >
-                  <li>
-                    <a>1 TNS</a>
-                  </li>
-                  <li>
-                    <a>1 SHARK</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="btn btn-sm btn-error btn-circle ml-auto">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
+
+            <div className="flex flex-row justify-between items-center">
+              <p>Token Check</p>
+              <div className="flex flex-row items-center gap-2 ml-auto">
+                <div className="dropdown">
+                  <label tabIndex={0} className="hover:cursor-pointer">
+                    Eligible
+                  </label>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content menu p-2 shadow bg-neutral rounded-box w-48"
+                  >
+                    <li>
+                      <a>1 TNS</a>
+                    </li>
+                    <li>
+                      <a>1 SHARK</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="btn btn-sm btn-error btn-circle ml-auto">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
-            <p>Twitter Account</p>
-            <div className="flex flex-row items-center gap-2 ml-auto">
-              <p>@yungweez703</p>
-              <div className="btn btn-sm btn-success btn-circle">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
-                  />
-                </svg>
+
+            <div className="flex flex-row justify-between items-center">
+              <p>Twitter Account</p>
+              <div className="flex flex-row items-center gap-2 ml-auto">
+                <p>@yungweez703</p>
+                <div className="btn btn-sm btn-success btn-circle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
