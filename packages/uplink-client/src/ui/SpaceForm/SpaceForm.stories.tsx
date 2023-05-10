758f13af29b832d84c99516d8c2e88810ef346b0
jooshwelsh <josh_welsh10@yahoo.com>
2023-05-10 17:22:05 -0400

    stretch admin input fields in edit space

--- packages/uplink-client/src/ui/SpaceForm/SpaceForm.stories.tsx
+++ packages/uplink-client/src/ui/SpaceForm/SpaceForm.stories.tsx
@@ -10,7 +10,7 @@ const Template = (args: any) => <SpaceForm {...args} />;
 
 export const CreateSpaceLoggedIn = Template.bind({});
 CreateSpaceLoggedIn.args = {
-  loggedIn: false,
+  loggedIn: true,
   isNewSpace: true,
   initialState: {
     name: "",
@@ -27,7 +27,7 @@ CreateSpaceLoggedIn.args = {
 
 export const CreateSpaceLoggedOut = Template.bind({});
 CreateSpaceLoggedOut.args = {
-  loggedIn: true,
+  loggedIn: false,
   isNewSpace: true,
   initialState: {
     name: "",
@@ -38,6 +38,27 @@ CreateSpaceLoggedOut.args = {
     admins: ["you", ""],
     errors: {
       admins: [],
+    },
+  },
+};
+
+export const CreateSpaceError = Template.bind({});
+CreateSpaceError.args = {
+  loggedIn: false,
+  isNewSpace: true,
+  initialState: {
+    name: "",
+    logoBlob: "",
+    logoUrl: "",
+    website: "",
+    twitter: "",
+    admins: ["you", "xyz1234567890", "abc1234567890", "0x02d1b331C8Ab3dDdC27Ef4EE891F264e5e3a4435", "ndodson.eth"],
+    errors: {
+      name: "Name is required",
+      logoUrl: "Logo is required",
+      website: "Website is required",
+      twitter: "Twitter is required",
+      admins: [null, "Invalid address", "Invalid address", null, null],
     },
   },
 };
