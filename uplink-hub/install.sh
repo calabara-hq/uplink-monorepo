27771d60048495e2296416c4de56d3f648b42ea9
nick <nick.m.dodson@gmail.com>
2023-06-21 17:24:11 -0400

    update gitignore rule to not ignore helm router charts, update router installation script

--- uplink-hub/install.sh
+++ uplink-hub/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ./packages/graphql-api/supergraph && curl -sSL https://router.apollo.dev/download/nix/latest | sh 
+cd ./packages/graphql-api/supergraph && curl -sSL https://router.apollo.dev/download/nix/v1.20.0 | sh 
