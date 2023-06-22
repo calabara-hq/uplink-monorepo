29e9181858e1579628c2108a1171299537700e3e
nick <nick.m.dodson@gmail.com>
2023-06-21 20:37:45 -0400

    add kubernetes scripting

--- uplink-hub/packages/graphql-api/scripts/subgraphs/kubernetes-networking.sh
+++ uplink-hub/packages/graphql-api/scripts/subgraphs/kubernetes-networking.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-subgraphs=("spaces")
+subgraphs=("spaces" "contests" "submit" "vote")
 
 url_spaces="http://spaces:8080"
+url_contests="http://contests:8080"
+url_submit="http://submit:8080"
+url_vote="http://vote:8080"
 
 schema_spaces="subgraphs/spaces/schema.graphql"
+schema_contests="subgraphs/contests/schema.graphql"
+schema_submit="subgraphs/submit/schema.graphql"
+schema_vote="subgraphs/vote/schema.graphql"
