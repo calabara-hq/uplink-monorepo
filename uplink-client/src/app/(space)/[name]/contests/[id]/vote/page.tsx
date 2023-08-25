99d6e5c9dce0cab05ad56955fc316afa09a98335
jooshwelsh <josh_welsh10@yahoo.com>
2023-08-25 14:45:05 -0400

    Mobile Vote Screen-- sizing & back button

--- uplink-client/src/app/(space)/[name]/contests/[id]/vote/page.tsx
+++ uplink-client/src/app/(space)/[name]/contests/[id]/vote/page.tsx
@@ -1,5 +1,6 @@
 import { VoteTab } from "@/ui/Contests/Vote";
 import { getContestById } from "../fetchContest";
+import { HiArrowNarrowLeft } from "react-icons/hi";
 
 // this page offers an expanded view of the voting process for a given contest.
 // it's mainly used for mobile devices only, but can be called from desktop as well.
@@ -22,9 +23,17 @@ export default async function Page({
     tweetId,
   } = await getContestById(params.id);
 
+
   return (
-    <div className="w-full sm:w-8/12 flex flex-col justify-center border-2 border-border rounded-lg">
-      <VoteTab contestId={params.id} votingPolicy={votingPolicy} />
+    <div className="w-full flex flex-col items-center justify-center gap-4 ">
+      <button className="btn btn-ghost mr-auto">
+        <HiArrowNarrowLeft className="h-6 w-6 mr-1" />
+        <a href={`/${params.name}/contests/${params.id}`}>Back to Contest</a>
+      </button>
+
+      <div className=" w-full md:w-3/4 lg:w-2/5 flex flex-col items-center justify-center border-2 border-border rounded-lg">
+        <VoteTab contestId={params.id} votingPolicy={votingPolicy} />
+      </div>
     </div>
   );
 }
