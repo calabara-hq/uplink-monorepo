878cc54eb747be0e2052cdac7e69eb4719626717
nick <nick.m.dodson@gmail.com>
2023-10-09 11:55:28 -0400

    remove cache headers

--- uplink-client/next.config.js
+++ uplink-client/next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// https://vercel.com/docs/edge-network/caching#how-to-cache-responses
-const edgeCacheHeader = {
-  // key: "Vercel-CDN-Cache-Control",
-  // value: "s-maxage=86400"
-  key: 'dummy',
-  value: 'dummy'
-}
-
 const nextConfig = {
   reactStrictMode: false,
   experimental: {
@@ -45,54 +37,6 @@ const nextConfig = {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
-  async headers() {
-    return [
-      {
-        source: "/",
-        headers: [edgeCacheHeader]
-      },
-      {
-        source: "/submission/:submissionId",
-        headers: [edgeCacheHeader]
-      },
-      {
-        source: "/:name",
-        headers: [edgeCacheHeader]
-      },
-      {
-        source: "/:name/contest/:id",
-        headers: [edgeCacheHeader]
-      },
-      {
-        source: "/:name/contest/:id/submission/:submissionId",
-        headers: [edgeCacheHeader]
-      },
-      {
-        source: "/:name/contest/:id/studio",
-        headers: [edgeCacheHeader]
-      },
-      {
-        source: "/:name/contest/:id/vote",
-        headers: [edgeCacheHeader]
-      },
-      {
-        source: "/:name/contest/create",
-        headers: [edgeCacheHeader]
-      },
-      {
-        source: "/spacebuilder/create",
-        headers: [edgeCacheHeader]
-      },
-      {
-        source: "/spacebuilder/edit/[name]",
-        headers: [edgeCacheHeader]
-      },
-      {
-        source: "/spaces",
-        headers: [edgeCacheHeader]
-      },
-    ]
-  }
 }
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
