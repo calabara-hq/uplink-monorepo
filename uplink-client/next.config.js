7336a126200cdf2b3c3e656d8318c1a3d39cbf86
nick <nick.m.dodson@gmail.com>
2023-10-09 12:31:35 -0400

    fix mobile walletconnectv2 config

--- uplink-client/next.config.js
+++ uplink-client/next.config.js
@@ -35,9 +35,12 @@ const nextConfig = {
   },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
 }
+
+
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
